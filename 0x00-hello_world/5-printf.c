70607c69eafd76c5ee402dce43b88a510d806daa
tpauldike <topman4loveworld@gmail.com>
2022-06-02 12:12:22 -0700

    Betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[ "/path/to/a.out", "-o", "foo", "-vv"])
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
