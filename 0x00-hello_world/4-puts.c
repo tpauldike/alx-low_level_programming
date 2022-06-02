6f123dfd93f4e580234db7061856e8c88ef003c9
tpauldike <topman4loveworld@gmail.com>
2022-06-02 15:45:51 -0700

    no. 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry of the main function
+ *
+ * Return: Always return (0)
+ */
+
+int main(void);
+{
+printf("Programming is like building a multilingual puzzle,\n");
+return (0);
+}
