206a306d2d9ed66602a87c16c909c2fb71a9f263
tpauldike <topman4loveworld@gmail.com>
2022-06-02 18:07:16 -0700

    Intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always return (0)
+ */
+
+ init main() {
+     init a = 16;
+     printf("Size of variable a: %d\n", sizeof (a));
+     printf("Size of int data type %d\n", sizeof(int));
+     printf("Size of char data type: %d \n", sizeof(char)):
+     printf"Size of float data type:%d\n", Sizeof(float));
+ printf("Size of double data type %d\n", sizeof (double));
+ return 0;
+ }
