6674542ef748cba3474f68dcd7b4aa0741ff84cb
tpauldike <topman4loveworld@gmail.com>
2022-06-02 19:48:03 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 	return (1);
 }
