742a1528e977989e3a08cb79ad66a778c1b1a11b
tpauldike <topman4loveworld@gmail.com>
2022-06-03 17:41:45 -0700

    betty & I

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	ini num1, num2, num3;
+
+	for (num1 = '0'; num1 < '9'; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		{
+			for (num3 = num2 + 1; num3 <= '9'; num3++)
+			{
+				if ((num2 != num1) != num3)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(num3);
+					if (num1 == '7' && num2 == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		putchar('\n');
+		return (0);
+
+	}
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,29 @@
-77;30601;0c
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: 0
+  */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 98; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
