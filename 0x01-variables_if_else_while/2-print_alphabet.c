f0260c3aaa07513621714f2ca7e788438e25d140
tpauldike <topman4loveworld@gmail.com>
2022-06-05 06:39:19 -0700

    Question 2, answered

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char Sweetheart;
+
+	for (Sweetheart = 'a'; Sweetheart <= 'z'; ++Sweetheart)
+		putchar (Sweetheart);
+	putchar ('\n');
+	return (0);
+}
