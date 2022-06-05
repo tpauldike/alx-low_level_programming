ef3155b3e792758eae4b75d57b1fadad14fb05d9
tpauldike <topman4loveworld@gmail.com>
2022-06-05 06:59:26 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n_
+
+		for (n_ = 0; n_ < 10; n_++)
+			printf("%i", n_);
+	putchar ('\n');
+	return (0);
+}
