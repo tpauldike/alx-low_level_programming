11e833073bec0600e79e015d884427cebf5adb26
tpauldike <topman4loveworld@gmail.com>
2022-06-06 15:51:57 -0700

    9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio>
+
+/**
+ * main - Entru [oint
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+
+		if (ch != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
