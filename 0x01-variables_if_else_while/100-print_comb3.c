0ba62e038ae37bd3bd1b42a1ad08d2e8eaa971ef
tpauldike <topman4loveworld@gmail.com>
2022-06-06 15:25:20 -0700

    task 100 done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,19 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
-  * main - Entry point
-  * Return: 0
-  */
+ * main - Entry point
+ * Return: 0
+ */
+
 int main(void)
 {
-    int y;
+	int x, y;
 
-    for (y = 0; y <= 98; y++)
-    {
-            putchar((y / 10) + '0');
-            putchar((y % 10) + '0');
-            putchar(',');
-            putchar(' ');
-    }
-    putchar('\n');
-    return (0);
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			putchar(x);
+		putchar(y);
+		if (x < '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
