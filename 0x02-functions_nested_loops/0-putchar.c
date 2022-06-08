4fc494dbb0b63360465673459a3f51c2830b15d2
tpauldike <topman4loveworld@gmail.com>
2022-06-07 18:04:01 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the something
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar (');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
