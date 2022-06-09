3832c5b67f1d5c6282314d3f80c99748d649e693
tpauldike <topman4loveworld@gmail.com>
2022-06-09 02:26:17 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet in ten lines
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char abc;
+	int i;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+		_putchar (abc);
+
+		i = 0;
+
+		for (i <= 10)
+		{
+			_putchar ('\n');
+			i++;
+		}
+	}
+}
