0074ae0db08c679ba3382bf1faca040d7a46a9d3
tpauldike <topman4loveworld@gmail.com>
2022-06-10 06:19:06 -0700

    More_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,13 +10,10 @@
 
 int _isupper(int c)
 {
-	if (c == 65; c <= 122)
+	if (c >= 65; c <= 90)
 	{
-		if (c <= 90)
-		{
-			return (1);
-		}
-		else
-			return (0);
+		return (1);
 	}
+	else
+		return (0);
 }
