73723f03ac7c44872548aa5de893f7a85a39fdc3
tpauldike <topman4loveworld@gmail.com>
2022-06-09 21:33:22 -0700

    ASDFGJK./

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,6 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int day);
+void print_remaining_days(int day, int month, int year);
 
 #endif /* MAIN_H */
