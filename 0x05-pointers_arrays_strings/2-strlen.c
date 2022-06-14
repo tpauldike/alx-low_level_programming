04b472357f7b21e43ed8a7a5fa6381372e50e1c7
tpauldike <topman4loveworld@gmail.com>
2022-06-14 11:13:50 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - A functiom that returns the length of a d'string
+ * @s: The pointer to the string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	char a[];
+	char *s = &a[];
+
+	int len = sizeof(a) / sizeof(a[0]);
+}
