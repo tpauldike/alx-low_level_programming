770aa3be913d5f1fd4b35cde386ab2ddca37699c
tpauldike <topman4loveworld@gmail.com>
2022-06-20 17:12:21 -0700

    task 1: The _memset function that fills the first n bytes of the memory poin

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _memset - fills the memory with a constant bite
+  * @n: The specified number of bytes
+  * @s: The pointer of type char
+  * @b: The constant byte
+  * Return: Returns a pointer to the memory area 's'
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	(s, 'b', n);
+	return (s);
+}
