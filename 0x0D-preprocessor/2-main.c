935a42bd331fe2e96f0229b648d92d0c74bf1b50
tpauldike <topman4loveworld@gmail.com>
2022-07-05 11:07:38 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+/* A program that prints the name of the it was compiled with */
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0
+ */
+void main(void)
+{
+	printf("This file is named: %s". _FILE_);
+
+	return (0);
+}
