54a7fd2549286c4921e0500b4bf3e18ea847ab2b
tpauldike <topman4loveworld@gmail.com>
2022-07-05 20:47:22 -0700

    typedef for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,11 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - the new name for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
