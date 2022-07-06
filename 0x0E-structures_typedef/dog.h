8f46997f5d3dead1b473ffff792037da8e838a9f
tpauldike <topman4loveworld@gmail.com>
2022-07-05 20:28:34 -0700

    README and first task done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - Informs the user about the dog
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: A structure in C, named dog that stores some information
+ * about any dog, consisting of; the name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
