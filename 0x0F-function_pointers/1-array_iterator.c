47cdec9918b333183742994f6aaf060d62236fa1
tpauldike <topman4loveworld@gmail.com>
2022-07-06 19:22:49 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+/* A function that executes a function given as a parpmeter on each elements of
+   an array. */
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes the funtion given to @array on each @size elements
+ * @array: An array of integers
+ * @size: The size of the array
+ * @action: The function pointer
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && action)
+		malloc(sizeof(size));
+		for (i = 0; i <= size; i++)
+			action(array[i]);
+}
