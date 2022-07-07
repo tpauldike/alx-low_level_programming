f356c9ce08e18258e7a97e24bd9a18aa6a973f66
tpauldike <topman4loveworld@gmail.com>
2022-07-06 22:04:56 -0700

    task 3 corrected

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include "3-calc.h"
 
 /**
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,6 +3,7 @@
  * respectively.
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 
 /**
