a1908e2010a59529930c7187261274c607f0f437
tpauldike <topman4loveworld@gmail.com>
2022-07-06 21:58:45 -0700

    A pointer that is an array of functions

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - An array of 5 functions
+ * @s: the pointer the holds the user's input
+ * Return: pointer to the function that correspoonds with the operation
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (s[0] == ops->op[i])
+			break;
+		i++;
+	}
+
+	return (ops[i / 2].f);
+}
