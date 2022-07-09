6a5a927566531cece11cb8fe56686d3f2218a1ef
tpauldike <topman4loveworld@gmail.com>
2022-07-08 18:22:21 -0700

    The functions redefined

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -14,71 +14,61 @@
  */
 int op_add(int a, int b)
 {
-	int sum = a + b;
-
-	return (sum);
+	return (a + b);
 }
 
 /**
  * op_sub - finds the difference between two integers
  * @a: the first integer
  * @b: the second integer
- * Return: the difference - sub - between @a and @b.
+ * Return: the difference between @a and @b.
  */
 int op_sub(int a, int b)
 {
-	int sub = a - b;
-
-	return (sub);
+	return (a - b);
 }
 
 /**
  * op_mul - computes the product of two integers
  * @a: the first integer
  * @b: the second integer
- * Return: the product - mul - of @a and @b.
+ * Return: the product of @a and @b.
  */
 int op_mul(int a, int b)
 {
-	int mul = a * b;
-
-	return (mul);
+	return (a * b);
 }
 
 /**
  * op_div - divides an integer by another integer
  * @a: the dividend (the integer to be divided)
  * @b: the divisor
- * Return: the quotient - div - of @a and @b.
+ * Return: the quotient of @a and @b.
  */
 int op_div(int a, int b)
 {
-	int div = a / b;
-
 	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	return (div);
+	return (a / b);
 }
 
 /**
  * op_mod - computes the remainder from the division of @a by @b
  * @a: the dividend (the integer that is divided)
  * @b: the divisor (the integer that divides
- * Return: the remainder - mod - from @a divided by @b.
+ * Return: the remainder from @a divided by @b.
  */
 int op_mod(int a, int b)
 {
-	int mod = a % b;
-
 	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	return (mod);
+	return (a % b);
 }
