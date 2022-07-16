5333ee9f3303a191e80b0ecd18453c096aac2516
tpauldike <topman4loveworld@gmail.com>
2022-07-15 21:30:36 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,17 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
  *
  * Return: The number of nodes in h.
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
+
 	while (h)
 	{
 		if (h->str == NULL)
@@ -18,5 +21,6 @@ size_t print_list(const list_t *h)
 		nodes++;
 		h = h->next;
 	}
+
 	return (nodes);
 }
