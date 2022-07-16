4f47811d4b211574d5598a015b0f22b59e299fc7
tpauldike <topman4loveworld@gmail.com>
2022-07-15 21:42:01 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,17 +13,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t nd;
 
-	while (h)
+	nd = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
 		h = h->next;
+		nd++;
 	}
-
-	return (nodes);
+	return (nd);
 }
