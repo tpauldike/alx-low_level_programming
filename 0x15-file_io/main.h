07bc692db71d2758bcf5a4386622a8e444fb259f
tpauldike <topman4loveworld@gmail.com>
2022-07-30 01:51:23 +0100

    header file corrected

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,9 +13,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-/**
- * ELF header functions
-*/
+/*ELF header functions*/
+
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
